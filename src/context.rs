3c2d297ffd5a4a000f8132f32821b8303ec28d26
krishpranav <krisna.pranav@gmail.com>
2021-12-25 19:17:36 +0530

    context: many context functions

--- src/context.rs
+++ src/context.rs
@@ -3,7 +3,7 @@ use crate::{Flag, FlagType, FlagValue};
 
 pub struct Context {
     pub args: Vec<String>,
-    flags: Option<Vec<(String, Result<FlagValue, FlagError>)>>,
+    flags: Option<Vec<(String, Result<FlagValue, Error>)>>,
     help_text: String,
 }
 
@@ -28,7 +28,7 @@ impl Context {
                         };
                         v.push((flag.name.to_string(), flag.value(val)))
                     } else {
-                        v.push((flag.name.to_string(), Err(FlagError::NotFound)))
+                        v.push((flag.name.to_string(), Err(Error::NotFound)))
                     }
                 }
                 Some(v)
@@ -43,7 +43,7 @@ impl Context {
         }
     }
 
-    fn result_flag_value(&self, name: &str) -> Result<FlagValue, FlagError> {
+    fn result_flag_value(&self, name: &str) -> Result<FlagValue, Error> {
         let flag = self
             .flags
             .as_ref()
@@ -54,7 +54,7 @@ impl Context {
                 Ok(val) => Ok(val.to_owned()),
                 Err(e) => Err(e.to_owned()),
             },
-            None => Err(FlagError::Undefined),
+            None => Err(Error::Undefined),
         }
     }
 
@@ -66,31 +66,86 @@ impl Context {
         }
     }
 
-    pub fn string_flag(&self, name: &str) -> Result<String, FlagError> {
+    pub fn string_flag(&self, name: &str) -> Result<String, Error> {
         let r = self.result_flag_value(name)?;
         match r {
             FlagValue::String(val) => Ok(val),
-            _ => Err(FlagError::TypeError),
+            _ => Err(Error::TypeError),
         }
     }
 
-    pub fn int_flag(&self, name: &str) -> Result<isize, FlagError> {
+    pub fn int_flag(&self, name: &str) -> Result<isize, Error> {
         let r = self.result_flag_value(name)?;
         match r {
             FlagValue::Int(val) => Ok(val),
-            _ => Err(FlagError::TypeError),
+            _ => Err(Error::TypeError),
         }
     }
 
-    pub fn float_flag(&self, name: &str) -> Result<f64, FlagError> {
+    pub fn float_flag(&self, name: &str) -> Result<f64, Error> {
         let r = self.result_flag_value(name)?;
         match r {
             FlagValue::Float(val) => Ok(val),
-            _ => Err(FlagError::TypeError),
+            _ => Err(Error::TypeError),
         }
     }
 
     pub fn help(&self) {
         println!("{}", self.help_text);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::error::Error;
+    use crate::{Context, Flag, FlagType};
+
+    #[test]
+    fn context_test() {
+        let args = vec![
+            "cli".to_string(),
+            "command".to_string(),
+            "args".to_string(),
+            "--bool".to_string(),
+            "--string".to_string(),
+            "test".to_string(),
+            "--int".to_string(),
+            "100".to_string(),
+            "--float".to_string(),
+            "1.23".to_string(),
+            "--invalid_float".to_string(),
+            "invalid".to_string(),
+        ];
+        let flags = vec![
+            Flag::new("bool", FlagType::Bool),
+            Flag::new("string", FlagType::String),
+            Flag::new("int", FlagType::Int),
+            Flag::new("float", FlagType::Float),
+            Flag::new("invalid_float", FlagType::Float),
+            Flag::new("not_specified", FlagType::String),
+        ];
+        let context = Context::new(args, Some(flags), "".to_string());
+
+        assert_eq!(context.bool_flag("bool"), true);
+        assert_eq!(context.string_flag("string"), Ok("test".to_string()));
+        assert_eq!(context.int_flag("int"), Ok(100));
+        assert_eq!(context.float_flag("float"), Ok(1.23));
+
+        assert_eq!(context.int_flag("string"), Err(Error::TypeError));
+
+        assert_eq!(
+            context.float_flag("invalid_float"),
+            Err(Error::ValueTypeError)
+        );
+
+        assert_eq!(
+            context.string_flag("not_registered"),
+            Err(Error::Undefined)
+        );
+
+        assert_eq!(
+            context.string_flag("not_specified"),
+            Err(Error::NotFound)
+        );
     }
 }
