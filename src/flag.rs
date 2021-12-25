61db25c3b820030e19a6328f17a73869d3594a45
krishpranav <krisna.pranav@gmail.com>
2021-12-25 18:53:35 +0530

    flag: flag value + flag type

--- src/flag.rs
+++ src/flag.rs
@@ -7,3 +7,23 @@ pub struct Flag {
     pub flag_type: FlagType,
     pub alias: Option<Vec<String>>,
 }
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum FlagType {
+    Bool,
+    String,
+    Int,
+    Float,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum FlagValue {
+    Bool(bool),
+    String(String),
+    Int(isize),
+    Float(f64),
+}
+
+impl Flag {
+    
+}
