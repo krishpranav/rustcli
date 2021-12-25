f1d38d3b5c24c2ea68c2c8468eae5d29c6c43af7
krishpranav <krisna.pranav@gmail.com>
2021-12-25 17:25:05 +0530

    error: different error msg
    added different error msg

--- src/error.rs
+++ src/error.rs
@@ -9,3 +9,15 @@ pub enum Errors {
     ValueTypeError,
     ArgumentError,
 }
+
+impl fmt::Display for Errors {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Errors::NotFound => f.write_str("NotFound"),
+            Errors::Undefined => f.write_str("Undefined"),
+            Errors::TypeError => f.write_str("TypeError"),
+            Errors::ValueTypeError => f.write_str("ValueTypeError"),
+            Errors::ArgumentError => f.write_str("ArgumentError"),
+        }
+    }
+}
