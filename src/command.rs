dbac55dbd5c3db20c543e97dd5199346fe27ad18
krishpranav <krisna.pranav@gmail.com>
2021-12-26 12:55:43 +0530

    command: helptext + Help command + test cases
    flag help text + help for command + cfg test cases to run command

--- src/command.rs
+++ src/command.rs
@@ -5,13 +5,13 @@ pub struct Command {
     pub name: String,
     pub description: Option<String>,
     pub usage: Option<String>,
-    pub action: Option<Stirng>,
-    pub flags: Option<String>,
-    pub alias: Option<String>,
+    pub action: Option<Action>,
+    pub flags: Option<Vec<Flag>>,
+    pub alias: Option<Vec<String>>,
 }
 
 impl Command {
-    pub fn new<T: Into<STring>>(name: T) -> Self {
+    pub fn new<T: Into<String>>(name: T) -> Self {
         Self {
             name: name.into(),
             ..Self::default()
@@ -44,7 +44,7 @@ impl Command {
 
     pub fn alias<T: Into<String>>(mut self, name: T) -> Self {
         if let Some(ref mut alias) = self.alias {
-            (*alias).push(name.into())
+            (*alias).push(name.into());
         } else {
             self.alias = Some(vec![name.into()]);
         }
@@ -60,5 +60,109 @@ impl Command {
             Some(action) => action(&Context::new(args, self.flags.clone(), self.help_text())),
             None => self.help(),
         }
+    }
+
+    fn flag_help_text(&self) -> String {
+        let mut text = String::new();
+        text += "Flags:\n";
+        let help_flag = "-h, --help";
+
+        if let Some(flags) = &self.flags {
+            let int_val = "<int>";
+            let float_val = "<float>";
+            let string_val = "<string>";
+
+            let flag_helps = &flags.iter().map(|f| {
+                let alias = match &f.alias {
+                    Some(alias) => alias
+                        .iter()
+                        .map(|a| format!("-{}", a))
+                        .collect::<Vec<String>>()
+                        .join(", "),
+                    None => String::new(),
+                };
+                let val = match f.flag_type {
+                    FlagType::Int => int_val,
+                    FlagType::Float => float_val,
+                    FlagType::String => string_val,
+                    _ => "",
+                };
+
+                let help = if alias.is_empty() {
+                    format!("--{} {}", f.name, val)
+                } else {
+                    format!("{}, --{} {}", alias, f.name, val)
+                };
+
+                (help, f.description.clone())
+            });
+
+            let flag_name_max_len = flag_helps
+                .clone()
+                .map(|h| h.0.len())
+                .chain(vec![help_flag.len()].into_iter())
+                .max()
+                .unwrap();
+
+            for flag_help in flag_helps.clone().into_iter() {
+                text += &format!("\t{}", flag_help.0);
+
+                if let Some(usage) = &flag_help.1 {
+                    let flag_name_len = flag_help.0.len();
+                    text += &format!(
+                        "{} : {}\n",
+                        " ".repeat(flag_name_max_len - flag_name_len),
+                        usage
+                    );
+                }
+            }
+
+            text += &format!(
+                "\t{}{} : Show help\n",
+                help_flag,
+                " ".repeat(flag_name_max_len - help_flag.len())
+            );
+        } else {
+            text += &format!("\t{} : Show help\n", help_flag);
+        }
+
+        text
+    }
+}
+
+impl Help for Command {
+    fn help_text(&self) -> String {
+        let mut text = String::new();
+
+        if let Some(description) = &self.description {
+            text += &format!("Description:\n\t{}\n\n", description);
+        }
+
+        if let Some(usage) = &self.usage {
+            text += &format!("Usage:\n\t{}\n\n", usage);
+        }
+
+        text += &self.flag_help_text();
+
+        text
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Action, Command, Context, Flag, FlagType};
+
+    #[test]
+    fn command_test() {
+        let a: Action = |c: &Context| println!("Hello, {:?}", c.args);
+        let c = Command::new("hello")
+            .description("usre command")
+            .usage("test hello user")
+            .alias("c")
+            .action(a)
+            .flag(Flag::new("t", FlagType::Bool));
+
+        assert_eq!(c.name, "hello".to_string());
+        assert_eq!(c.usage, Some("test hello user".to_string()));
     }
 }
